28c60235cd088b71dca4e29959a48b327eed5135
Anastasiia34 <mainstreamainstream@gmail.com>
2021-11-23 14:41:24 +0200

    Initial Commit

---
+++ BoardGameClub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoardGameClub
+//
+//  Created by Анастасия Стрекалова on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
